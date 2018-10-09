38fd6dcc42cdad9006e6a2561a61ca2be5bcd527
riversun <riversun.org@gmail.com>
2018-10-09 20:18:25 +0900

    Add webpack.config.js Move index.html into public

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+module.exports = {
+    mode: 'development',
+    devServer: {
+        open: true,
+        openPage: "index.html",
+        contentBase: path.join(__dirname, 'public'),
+        watchContentBase: true,
+        port: 8080,
+    }
+};
